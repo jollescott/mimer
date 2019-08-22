ed08f3f2d2c95679d9477a08d52c8082b8d77611
Joel Linder <jollescott.carl@hotmail.com>
2019-08-22 17:45:58 +0200

    Login logout working

--- mimer/quiz/urls.py
+++ mimer/quiz/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home', views.home, name='home'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout')
 ]
--- mimer/quiz/views.py
+++ mimer/quiz/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
-
+import django.contrib.auth
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        return redirect('login')
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    context = {}
+
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+
+        user = django.contrib.auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            django.contrib.auth.login(request, user)
+            return redirect('home')
+        else:
+            context = {
+                'fail': True
+            }
+
+    return render(request, 'quiz/login.html', context=context)    
+
+def logout(request):
+    django.contrib.auth.logout(request)
+    return redirect('index')
+
+def home(request):
+    return render(request, 'quiz/home.html')
