d536f6b35532545f62d2499411b1e54ddc4875de
Joel Linder <jollescott.carl@hotmail.com>
2019-08-22 20:06:20 +0200

    Added some relational stuff

--- mimer/quiz/admin.py
+++ mimer/quiz/admin.py
@@ -3,3 +3,5 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.QuizUser)
+admin.site.register(models.Test)
+admin.site.register(models.Question)
--- mimer/quiz/models.py
+++ mimer/quiz/models.py
@@ -2,5 +2,27 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
 class QuizUser(AbstractUser):
     overall_score = models.SmallIntegerField(default=0)
+    sana = models.BooleanField(default=False)
+
+class Question(models.Model):
+    text = models.CharField(max_length=50)
+
+    answer_a = models.CharField(max_length=50)
+    answer_b = models.CharField(max_length=50)
+    answer_c = models.CharField(max_length=50)
+    answer_d = models.CharField(max_length=50)
+
+    correct = models.CharField(max_length=1)
+
+    def __str__(self):
+        return self.text
+
+class Test(models.Model):
+    questions = models.ManyToManyField(Question)
+    user = models.ForeignKey(QuizUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} {1}".format(self.user, self.id)
