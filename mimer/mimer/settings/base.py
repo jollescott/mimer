812498fbd5a8225f2ccb4821a9cb05ac9322b897
Joel Linder <JoLi0125@student.grillska.se>
2019-09-20 09:52:23 +0200

    added collect static

--- mimer/mimer/settings/base.py
+++ mimer/mimer/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -97,7 +98,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static")),
-    
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 def load_sensitve(name):
     with open('mimer/settings/sensitive/{0}.json'.format(name), 'r') as config:
         return json.loads(config.read())
