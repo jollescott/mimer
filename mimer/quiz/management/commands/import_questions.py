1e9e9a6b70bb7a4bbba340918b1459d14b3aecca
Joel Linder <jollescott.carl@hotmail.com>
2019-10-13 18:15:07 +0200

    Improved docker ignore better init check

--- mimer/quiz/management/commands/import_questions.py
+++ mimer/quiz/management/commands/import_questions.py
@@ -23,7 +23,7 @@ class Command(BaseCommand):
         count = options['count']
 
         if init:
-            if len(Asset.objects.all()) > 0:
+            if Asset.objects.all().count() > 0:
                 print('Database has already been seeded!')
                 return
 
