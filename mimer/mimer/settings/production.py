9984ed93a7cf2fe3e7192a03f2d7cc93c3c20922
joel linder <Jollescott.carl@hotmail.com>
2019-11-09 22:25:29 +0100

    Sh*ts working again

--- mimer/mimer/settings/production.py
+++ mimer/mimer/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['frogor.herokuapp.com', 'frogor.joellinder.dev']
+ALLOWED_HOSTS = ['*']
 
 sensitive = load_sensitve('production')
 SECRET_KEY = sensitive['SECRET_KEY']
