b2bf938319250494f32c56a442ef0688c9324809
joel linder <Jollescott.carl@hotmail.com>
2019-11-09 22:26:25 +0100

    Updated hosts

--- mimer/mimer/settings/production.py
+++ mimer/mimer/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['frogor.herokuapp.com', 'frogor.joellinder.dev']
 
 sensitive = load_sensitve('production')
 SECRET_KEY = sensitive['SECRET_KEY']
